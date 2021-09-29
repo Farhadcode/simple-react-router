21881711f7c276df558b25f0d3f8cc0b5a479d35
Farhadcode <farhad.ahmed.aust@gmail.com>
2021-09-29 12:57:24 +0600

    react Router addad

---
+++ src/components/Abouts/Abouts.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Abouts = () => {
+    return (
+        <div>
+            <h3> He is  a  Good person </h3>
+            <p>You can konw all about </p>
+        </div>
+    );
+};
+
+export default Abouts;
